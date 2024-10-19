3704ac4730b2f4b2f9d742d9abc9688e98023411
rahul-754 <161922950+rahul-754@users.noreply.github.com>
2024-10-19 20:12:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<Register />} />
+                    <Route path="/" element={<Register />}  />
                     <Route path="/otpverification" element={<OTPVerification/>}/>
                     <Route path="/otpverified" element={<OTPVerified/>}/>
                     <Route path="/jobsform" element={<JobForm />} />
